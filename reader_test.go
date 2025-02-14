ed3bf3cd049cb3247baa2c8dda6dd4c0e9843793
Ben Barnard <barnardb@gmail.com>
2025-02-14 21:01:20 +0100

    Add test that readString doesn't give up after 1 Read

--- reader_test.go
+++ reader_test.go
@@ -18,3 +18,16 @@ func TestReadBuf(t *testing.T) {
 		t.Errorf("\nexpected: %#v\nbut got:  %#v", expected, bytes)
 	}
 }
+
+func TestReadString(t *testing.T) {
+	expected := "abcdefgh"
+	readerData := append([]byte{'U', 8}, expected...)
+	r := newBinaryReader(iotest.OneByteReader(bytes.NewReader(readerData)))
+	str, err := r.readString(8)
+	if err != nil {
+		t.Fatalf("failed to readBuf: %+v\n", err)
+	}
+	if str != expected {
+		t.Errorf("\nexpected: %#v\nbut got:  %#v", expected, str)
+	}
+}
